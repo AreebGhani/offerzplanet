76ca943645d130c6767ebb57d3f14f2fa460ba4e
Areeb Ghani <91328326+AreebGhani@users.noreply.github.com>
2023-07-13 00:44:17 +0500

    frontend

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 export const server = "https://offerzplanet-backend-38f34fca1550.herokuapp.com/api";
 
 export const backend_url = "https://offerzplanet-backend-38f34fca1550.herokuapp.com/";
-/
+
 export const socket_endpoint = "https://offerzplanet-socket-1ed6d6b57812.herokuapp.com/";

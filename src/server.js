b2ec72cbef5dfb69896f60f6d8ad2ff036bf612b
Areeb Ghani <91328326+AreebGhani@users.noreply.github.com>
2023-08-06 15:30:26 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-export const server = "http://ec2-44-202-155-191.compute-1.amazonaws.com/api";
+export const server = "http://ec2-44-201-242-159.compute-1.amazonaws.com/api";
 
-export const backend_url = "http://ec2-44-202-155-191.compute-1.amazonaws.com/";
+export const backend_url = "http://ec2-44-201-242-159.compute-1.amazonaws.com/";
 
 export const socket_endpoint = "https://offerzplanet-socket-io.vercel.app/";
